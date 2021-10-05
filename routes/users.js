7050ffdc9aceb86168562de872c6e9bf2632dea6
MWayneMercado <MWayneMercado@gmail.com>
2021-10-05 13:18:22 -0700

    router cleanup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const users_controller = require('../controllers/usersController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', users_controller.users_get);
 
 module.exports = router;
