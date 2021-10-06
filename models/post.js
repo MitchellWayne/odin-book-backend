e5b8acbb2d2ae37532465845dd6c912326e5c176
MWayneMercado <MWayneMercado@gmail.com>
2021-10-06 15:28:16 -0700

    remove array requirement for author

--- models/post.js
+++ models/post.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let postSchema = new Schema({
+  author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
   title: {type: String, required: true},
   text: {type: String, required: true},
   timestamp: {type: String, required: true},
   likes: [{type: Schema.Types.ObjectId, ref: 'User', required: false}],
-  author: [{type: Schema.Types.ObjectId, ref: 'User', required: true}],
   comments: [{type: Schema.Types.ObjectId, ref: 'Comment', required: false}],
 });
 
