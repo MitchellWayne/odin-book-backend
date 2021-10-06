0d5ee7380e94eaabdf677a9ae7143321ff43574b
MWayneMercado <MWayneMercado@gmail.com>
2021-10-06 15:23:08 -0700

    define basic user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const users_controller = require('../controllers/usersController');
+const user_controller = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', users_controller.users_get);
+// Get users listing
+router.get('/', user_controller.userlist_get);
+
+// Create new user
+router.get('/', user_controller.user_post);
+
+// Get user by ID
+router.get('/:userID', user_controller.user_get);
+
+// Update user by ID
+router.put('/:userID', user_controller.user_put);
+
+// Delete user by ID
+router.delete('/:userID', user_controller.user_delete);
 
 module.exports = router;
