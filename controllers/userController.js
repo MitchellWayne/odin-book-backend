df5e8ba21ffd5df484fd916d6039103174655ef8
MWayneMercado <MWayneMercado@gmail.com>
2021-10-07 14:40:20 -0700

    define userlist_get

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
-exports.userlist_get = function(req, res){
+const { body, validationResult } = require('express-validator');
 
+const User = require('../models/user');
+
+exports.userlist_get = function(req, res, next){
+  User.find()
+  .exec(function(err, userList){
+    if(err) return next(err);
+    if(!userList) return res.status(404).json({err: "could not retrieve user list or users DNE"});
+    return res.status(200).json(userList);
+  });
 };
 
 exports.user_post = function(req, res){
