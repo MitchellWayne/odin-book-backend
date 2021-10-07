7d9280f7d9c0f90c152725f8c27168d267b726a0
MWayneMercado <MWayneMercado@gmail.com>
2021-10-07 14:54:51 -0700

    define user_post

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
 const { body, validationResult } = require('express-validator');
+const passport = require('passport');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -11,9 +14,31 @@ exports.userlist_get = function(req, res, next){
   });
 };
 
-exports.user_post = function(req, res){
+exports.user_post = [
+  // Form validation
+  body('firstName', 'First name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('lastName', 'Last name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('userName', 'Username must not be empty.').trim().isLength({min: 1}).escape(),
+  body('password', 'Please enter a strong password of minimum length 8, at least one uppercase and lowercase letter, one number, and one symbol.').trim().isStrongPassword(),
+  body('confirmPassword', 'Passwords do not match.').trim().custom((value, { req }) => value === req.body.password),
 
-};
+  // (req, res, next) => {
+  //   const errors = validationResult(req);
+
+  //   bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
+  //     if (err) return next(err);
+  //     else {
+  //       const user = new User({
+  //         username: req.body.username,
+  //         password: hashedPassword,
+  //       }).save(err => {
+  //         if (err) return res.status(404).json({error: err});
+  //         return res.status(200).json({message: "successfully made user"});
+  //       });
+  //     }
+  //   });
+  // }
+];
 
 exports.user_get = function(req, res, next){
   User.findById(req.params.userID)
@@ -31,3 +56,5 @@ exports.user_put = function(req, res){
 exports.user_delete = function(req, res){
 
 };
+
+// Maybe seperate functions to get user posts, fullname, friends, and requests.
