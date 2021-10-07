30b3dfc622ba52ec07fc8a6be84cee59f3a981bc
MWayneMercado <MWayneMercado@gmail.com>
2021-10-07 16:56:42 -0700

    define base user_put (to be compartmentalized)

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+// Change this to only have fullnames + ID, reduce server load on MongoDB
 exports.userlist_get = function(req, res){
   User.find()
   .exec(function(err, userList){
@@ -43,6 +44,10 @@ exports.user_post = [
   }
 ];
 
+// Update to accept filtering:
+// See: https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/#h-maintain-good-security-practices
+// To implement, maybe create an option string to pass into findById to modify DB call
+// See: https://mongoosejs.com/docs/api.html#model_Model.findById
 exports.user_get = function(req, res){
   User.findById(req.params.userID)
   .exec(function (err, user){
@@ -52,12 +57,44 @@ exports.user_get = function(req, res){
   });
 };
 
-exports.user_put = function(req, res){
+// Split this into separate name, user, and password update APIs
+exports.user_put = [
+  // Form validation
+  body('firstName', 'First name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('lastName', 'Last name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('userName', 'Username must not be empty.').trim().isLength({min: 1}).escape(),
+  body('password', 'Please enter a strong password of minimum length 8, at least one uppercase and lowercase letter, one number, and one symbol.').trim().isStrongPassword(),
+  body('confirmPassword', 'Passwords do not match.').trim().custom((value, { req }) => value === req.body.password),
 
-};
+  (req, res) => {
+    const errors = validationResult(req);
+
+    bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
+      if(err) return res.status(404).json({err: err});
+      if(errors) return res.status(404).json({err: errors});
+
+      const user = new User({
+        firstname: req.body.firstName,
+        lastname: req.body.lastName,
+        username: req.body.userName,
+        password: hashedPassword,
+        _id: req.body.userid
+      });
+
+      User.findByIdAndUpdate(req.params.userid, user, {}, function(err, updatedUser){
+        if (err) return next(err);
+        return res.status(200).json({message: "user updated successfully"});
+      });
+    });
+  }
+];
 
 exports.user_delete = function(req, res){
 
 };
 
-// Maybe seperate functions to get user posts, fullname, friends, and requests.
+// Add user_login and user_logout
+
+// Instead of a userfriend_post, update the friend field through the request API
+// let the request API take the target user ID and a boolean to determine whether to
+// add the target user to the current user's friendlist
