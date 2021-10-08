e6d55227b643c5ef65fe5489843b0f117080a4d5
MWayneMercado <MWayneMercado@gmail.com>
2021-10-08 12:45:48 -0700

    cleanup field names/add username to userlist query

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,37 +7,41 @@ const User = require('../models/user');
 
 // Get user list by fullnames + _id
 exports.userlist_get = function(req, res){
+  const { firstName, lastName } = req.query;
   User.find()
-  .select('firstName lastName')
+  .select('firstname lastname username')
   .exec(function(err, userList){
     if(err) return res.status(404).json({err: err});
     if(!userList) return res.status(404).json({err: "could not retrieve user list or users DNE"});
+
+
+    // if(firstName) userList = 
+
     return res.status(200).json(userList);
   });
 };
 
 exports.user_post = [
   // Form validation
-  body('firstName', 'First name must not be empty.').trim().isLength({min: 1}).escape(),
-  body('lastName', 'Last name must not be empty.').trim().isLength({min: 1}).escape(),
-  body('userName', 'Username must not be empty.').trim().isLength({min: 1}).escape(),
+  body('firstname', 'First name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('lastname', 'Last name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('username', 'Username must not be empty.').trim().isLength({min: 1}).escape(),
   body('password', 'Please enter a strong password of minimum length 8, at least one uppercase and lowercase letter, one number, and one symbol.').trim().isStrongPassword(),
-  body('confirmPassword', 'Passwords do not match.').trim().custom((value, { req }) => value === req.body.password),
 
   (req, res) => {
     const errors = validationResult(req);
 
     bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
       if(err) return res.status(404).json({err: err});
-      if(errors) return res.status(404).json({err: errors});
+      if(!errors.isEmpty()) return res.status(404).json({err: errors});
       else {
         const user = new User({
-          firstname: req.body.firstName,
-          lastname: req.body.lastName,
-          username: req.body.userName,
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          username: req.body.username,
           password: hashedPassword,
-        }).save(err => {
-          if (err) return res.status(404).json({error: err});
+        }).save(saveErr => {
+          if (saveErr) return res.status(404).json({err: saveErr});
           return res.status(200).json({message: "successfully made user"});
         });
       }
@@ -45,10 +49,6 @@ exports.user_post = [
   }
 ];
 
-// Update to accept filtering:
-// See: https://stackoverflow.blog/2020/03/02/best-practices-for-rest-api-design/#h-maintain-good-security-practices
-// To implement, maybe create an option string to pass into findById to modify DB call
-// See: https://mongoosejs.com/docs/api.html#model_Model.findById
 exports.user_get = function(req, res){
   User.findById(req.params.userID)
   .exec(function (err, user){
@@ -61,9 +61,9 @@ exports.user_get = function(req, res){
 // Split this into separate name, user, and password update APIs
 exports.user_put = [
   // Form validation
-  body('firstName', 'First name must not be empty.').trim().isLength({min: 1}).escape(),
-  body('lastName', 'Last name must not be empty.').trim().isLength({min: 1}).escape(),
-  body('userName', 'Username must not be empty.').trim().isLength({min: 1}).escape(),
+  body('firstname', 'First name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('lastname', 'Last name must not be empty.').trim().isLength({min: 1}).escape(),
+  body('username', 'Username must not be empty.').trim().isLength({min: 1}).escape(),
   body('password', 'Please enter a strong password of minimum length 8, at least one uppercase and lowercase letter, one number, and one symbol.').trim().isStrongPassword(),
   body('confirmPassword', 'Passwords do not match.').trim().custom((value, { req }) => value === req.body.password),
 
@@ -72,18 +72,18 @@ exports.user_put = [
 
     bcryptjs.hash(req.body.password, 10, (err, hashedPassword) => {
       if(err) return res.status(404).json({err: err});
-      if(errors) return res.status(404).json({err: errors});
+      if(!errors.isEmpty()) return res.status(404).json({err: errors});
 
       const user = new User({
-        firstname: req.body.firstName,
-        lastname: req.body.lastName,
-        username: req.body.userName,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
         password: hashedPassword,
         _id: req.body.userid
       });
 
-      User.findByIdAndUpdate(req.params.userid, user, {}, function(err, updatedUser){
-        if (err) return next(err);
+      User.findByIdAndUpdate(req.params.userid, user, {}, function(updateErr, updatedUser){
+        if (updateErr) return next(updateErr);
         return res.status(200).json({message: "user updated successfully"});
       });
     });
