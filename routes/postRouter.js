6ecd07203d451c431b3b20f016c0f312198fb2b2
MWayneMercado <MWayneMercado@gmail.com>
2021-10-15 13:49:19 -0700

    add user auth to some post routes

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const post_controller = require('../controllers/postController');
 
-// Get post listing
+// Get post listing from user
 router.get('/', post_controller.postlist_get);
 
-// Create new post
-router.get('/', post_controller.post_post);
+// Create new post for user
+router.post('/', passport.authenticate('jwt', {session: false}), post_controller.post_post);
 
 // Get post by ID
 router.get('/:postID', post_controller.post_get);
 
 // Update post by ID
-router.put('/:postID', post_controller.post_put);
+router.put('/:postID', passport.authenticate('jwt', {session: false}), post_controller.post_put);
 
 // Delete post by ID
-router.delete('/:postID', post_controller.post_delete);
+router.delete('/:postID', passport.authenticate('jwt', {session: false}), post_controller.post_delete);
 
 module.exports = router;
