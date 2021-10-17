e3814fd1c38ddb0e24312a6928558bbb94243cc6
MWayneMercado <MWayneMercado@gmail.com>
2021-10-17 13:25:46 -0700

    define post_put

--- controllers/postController.js
+++ controllers/postController.js
@@ -42,9 +42,30 @@ exports.post_get = function(req, res){
   });
 };
 
-exports.post_put = function(req, res){
+exports.post_put = [
+  body('title', 'Title must not be empty.').trim().isLength({min: 1}).escape(),
+  body('text', 'Text content must not be empty.').trim().isLength({min: 1}).escape(),
+  
+  (req, res) => {
+    if(req.user._id.toString() !== req.params.userID) {
+      return res.status(404).json({message: "user not authorized for different user endpoints"});
+    } else {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) return res.status(404).json({err: errors});
 
-};
+      const post = {
+        title: req.body.title,
+        text: req.body.text,
+        timestamp: Date.now(),
+      }
+
+      Post.findByIdAndUpdate(req.params.postID, post, {}, function(updateErr, updatedPost){
+        if (updateErr) return res.status(404).json({err: updateErr});
+        else res.status(201).json({message: "post updated successfully"});
+      });
+    }
+  }
+];
 
 exports.post_delete = function(req, res){
 
