e563c81fbdec7fb6d56ee13dbd2ee3f3e9c066bf
MWayneMercado <MWayneMercado@gmail.com>
2021-10-19 12:29:59 -0700

    comment routes prep and mergeParams: true

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router({mergeParams: true});
 
 const comment_controller = require('../controllers/commentController');
 
-// Get comments listing
+// Get comments listing from postID
 router.get('/', comment_controller.commentlist_get);
 
-// Create new comment
-router.get('/', comment_controller.comment_post);
-
-// Get comment by ID
+// Direct comment routes
+router.post('/', comment_controller.comment_post);
 router.get('/:commentID', comment_controller.comment_get);
-
-// Update comment by ID
 router.put('/:commentID', comment_controller.comment_put);
-
-// Delete comment by ID
 router.delete('/:commentID', comment_controller.comment_delete);
 
 module.exports = router;
