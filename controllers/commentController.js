d189b2f3b35c72bd8baf690105a3556e567d1d21
MWayneMercado <MWayneMercado@gmail.com>
2021-10-20 14:44:53 -0700

    define commentlist_get

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,5 +1,10 @@
 exports.commentlist_get = function(req, res){
-
+  Comment.find()
+  .exec(function(err, commentList){
+    if(err) return res.status(404).json({err: err});
+    if(!commentList) return res.status(404).json({err: "could not retrieve comment list or comments DNE"});
+    return res.status(200).json(commentList);
+  });
 };
 
 exports.comment_post = function(req, res){
