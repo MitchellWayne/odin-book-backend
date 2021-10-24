715f2471be742dd1327630424e1d57294acb58a7
MWayneMercado <MWayneMercado@gmail.com>
2021-10-24 15:01:51 -0700

    add jwt tokens to relevant comment routes

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const passport = require('passport');
 
 const comment_controller = require('../controllers/commentController');
 
@@ -7,9 +8,9 @@ const comment_controller = require('../controllers/commentController');
 router.get('/', comment_controller.commentlist_get);
 
 // Direct comment routes
-router.post('/', comment_controller.comment_post);
+router.post('/', passport.authenticate('jwt', {session: false}), comment_controller.comment_post);
 router.get('/:commentID', comment_controller.comment_get);
 // router.put('/:commentID', comment_controller.comment_put);
-router.delete('/:commentID', comment_controller.comment_delete);
+router.delete('/:commentID', passport.authenticate('jwt', {session: false}), comment_controller.comment_delete);
 
 module.exports = router;
